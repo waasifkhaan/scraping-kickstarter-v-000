23c7a55df5f5ac4add9574966ac4cc482770d51b
Wasif Khan <waasif.khaan@gmail.com>
2018-07-20 23:31:08 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,9 +20,29 @@ def create_project_hash
   # return the projects hash
   projects
 end
-# projects: kickstarter.css("li.project.grid_4")
-# title : project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-#funded : project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# project title :project =   kickstarter.css("li.project grid_4")
+                  title = project.css("h2.bbcard_name a target").text
+                 title-image : project.css("div.project-thumbnail a img").attribute("src").value
+                 
+                 location : project.css(a data-location span.location-name).text
